520a71dd83ca2d176ca13936df149c2885b10156
Enxos <23334313+Enxos@users.noreply.github.com>
2024-05-29 17:46:30 +0200

    FelugroEmberNezet bugfix...

---
+++ src/nezet/FelugroEmberNezet.java
@@ -0,0 +1,39 @@
+package nezet;
+
+import javax.swing.JOptionPane;
+import modell.EmberModell;
+
+public class FelugroEmberNezet implements EmberNezet {
+    
+    private EmberModell model;
+
+    public FelugroEmberNezet(EmberModell model) {
+        this.model = model;
+    }
+
+    @Override
+    public String getNev() {
+        return JOptionPane.showInputDialog("Név: ");
+    }
+
+    @Override
+    public String getSzuletesiEv() {
+        return JOptionPane.showInputDialog("Születési év: ");
+    }
+
+    @Override
+    public String getNem() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNem'");
+    }
+
+    @Override
+    public String getLakhely() {
+        return JOptionPane.showInputDialog("Lakhely: ");
+    }
+
+    @Override
+    public void megjelenito(String info) {
+        JOptionPane.showMessageDialog(null, info);
+    }
+}
